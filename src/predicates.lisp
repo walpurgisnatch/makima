78d13046c05cac1bacdeaccea0a44a81880a063e
Walpurgisnatch <Kawemirs@gmail.com>
2023-06-04 22:58:25 +0700

    comparsion predicates

---
+++ src/predicates.lisp
@@ -0,0 +1,30 @@
+(in-package :cl-user)
+(defpackage makima.predicates
+  (:use :cl :makima.utils)
+  (:import-from :pero
+                :write-log)
+  (:export :in-content
+           :more-than
+           :more-or-equal-than
+           :less-then
+           :less-or-equal-than))
+
+(in-package :makima.predicates)
+
+(defmacro string-as-float-comparsion (fun &rest args)
+  `(,fun ,@(loop for arg in args collect `(parse-float ,arg))))
+
+(defun in-content (content regex)
+  (cl-ppcre:scan-to-strings regex content))
+
+(defun more-than (content arg)
+  (string-as-float-comparsion > content arg))
+
+(defun more-or-equal-than (content arg)
+  (string-as-float-comparsion >= content arg))
+
+(defun less-than (content arg)
+  (string-as-float-comparsion < content arg))
+
+(defun less-or-equal-than (content arg)
+  (string-as-float-comparsion <= content arg))
